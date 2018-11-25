624a62977e75c4dc44b0c4f27c1bae700f544227
cs2dsb <cs2dsb@gmail.com>
2018-11-25 17:35:32 +0000

    Added some icons

--- src/systems/camera_movement.rs
+++ src/systems/camera_movement.rs
@@ -13,11 +13,13 @@ use ::{
     Matriarch,
     Walker,
     Direction,
+    Exit,
   },
   config::CameraConfig,
   resources::{
     Command,
     CommandChannel,
+    SpawnStats,
   },
 };
 
@@ -35,6 +37,8 @@ impl<'s> System<'s> for CameraMovement {
     ReadStorage<'s, Walker>,
     Read<'s, CameraConfig>,
     Read<'s, CommandChannel>,
+    Read<'s, SpawnStats>,
+    ReadStorage<'s, Exit>,
   );
 
   fn setup(&mut self, res: &mut Resources) {
@@ -42,7 +46,7 @@ impl<'s> System<'s> for CameraMovement {
     self.command_reader = Some(res.fetch_mut::<CommandChannel>().register_reader());
   }
 
-  fn run(&mut self, (time, mut transforms, fly_tags, matriarchs, walkers, camera_config, commands): Self::SystemData) {
+  fn run(&mut self, (time, mut transforms, fly_tags, matriarchs, walkers, camera_config, commands, spawn_stats, exits): Self::SystemData) {
     let delta = time.delta_seconds();
 
     let mut zoom = 0.0;
@@ -68,9 +72,37 @@ impl<'s> System<'s> for CameraMovement {
 
     if num_matriarchs > 0 {
       matriarch_translation /= num_matriarchs as f32;
+    }
+
+    let mut exit_translation = Vector3::new(0.0, 0.0, 0.0);
+    let mut num_exits = 0;
+
+    if spawn_stats.saved > 0 {
+      for (t, _exit) in (&transforms, &exits).join() {
+        num_exits += 1;
+        exit_translation += t.translation;
+      }
+
+      if num_exits > 0 {
+        exit_translation /= num_exits as f32;
+      }
+    }
+
+    //So now we've got the average positions of the matriarchs and exits
+    if num_exits > 0 || num_matriarchs > 0 {
+      let final_target = {
+        if num_matriarchs == 0 {
+          exit_translation
+        } else {
+          let num_exited = spawn_stats.saved as f32;
+          exit_translation *= num_exited;
+          (matriarch_translation + exit_translation) / (num_exited + 1.0) //+1 for the matriarch
+        }
+      };
+
       for (t, _tag) in (&mut transforms, &fly_tags).join() {
-        t.translation.x += (matriarch_translation.x - t.translation.x) * delta * camera_config.convergence_speed;
-        t.translation.y += (matriarch_translation.y - t.translation.y) * delta * camera_config.convergence_speed;
+        t.translation.x += (final_target.x - t.translation.x) * delta * camera_config.convergence_speed;
+        t.translation.y += (final_target.y - t.translation.y) * delta * camera_config.convergence_speed;
 
         t.translation.z += zoom * delta * camera_config.zoom_speed;
         t.translation.z = t.translation.z
